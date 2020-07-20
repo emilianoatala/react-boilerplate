4d967e3a7bda5cdb78aa054e760bb32418da7ebf
eatala <info@emiatala.com>
2020-07-20 19:04:53 -0300

    remove graphql base

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,6 @@ var path = require("path");
 const CompressionPlugin = require("compression-webpack-plugin");
 const BrotliPlugin = require("brotli-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -48,14 +46,12 @@ module.exports = {
       minRatio: 0.8,
     }),
     new BrotliPlugin({
-      //brotli plugin
       asset: "[path].br[query]",
       test: /\.(js|jsx||css|html|svg)$/,
       threshold: 0,
       minRatio: 0.8,
     }),
     new CleanWebpackPlugin(),
-    new BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "/static/index.html"),
@@ -86,12 +82,12 @@ module.exports = {
     filename: "app.bundle.js",
   },
 
-  // DEV SERVER ENTRY POINT
-  devServer: {
-    contentBase: path.resolve(__dirname, "./src"),
-    port: 3000,
-    watchContentBase: true,
-    open: true,
-    compress: true,
-  },
+  // // DEV SERVER ENTRY POINT
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, "./src"),
+  //   port: 3000,
+  //   watchContentBase: true,
+  //   open: true,
+  //   compress: true,
+  // },
 };
